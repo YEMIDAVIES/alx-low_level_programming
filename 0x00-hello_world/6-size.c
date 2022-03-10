14264be5bc22fd813e9468a1854ef82b9b8c44ce
Devdesiign <devdesiign@gmail.com>
2022-03-10 08:46:09 -0800

    All

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+short a;
+int b;
+long c;
+char d;
+float e;
+double f;
+
+printf("Size of Short: %lu \n", (unsigned long)sizeof(a));
+printf("Size of Int: %lu \n", (unsigned long)sizeof(b));
+printf("Size of Long: %lu \n", (unsigned long)sizeof(c));
+printf("Size of Char: %lu \n", (unsigned long)sizeof(d));
+printf("Size of Float: %lu \n", (unsigned long)sizeof(e));
+printf("Size of Double: %lu \n", (unsigned long)sizeof(f));
+
+return (0);
+}
