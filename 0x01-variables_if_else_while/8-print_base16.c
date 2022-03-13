2c7d380bfdb7f1b1f0c536e1d0e0de4387d5ebb3
Yemzy1 <yemidavies302@gmail.com>
2022-03-13 23:50:14 +0100

    C programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print all hexadecimal numbers
- * 
- * Return: Always 0 (sucess)
- *
+* main - print all hexadecimal numbers
+* 
+* Return: Always 0 (sucess)
+*
 */
 
 int main(void)
